eea17fe91786f007366fe4adf5db88f2916273ac
Sarah Stoffel <17sarah.stoffel@gmail.com>
2022-07-12 22:34:13 -0500

    complete PyBank and updated README

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+from numpy import average
+
 budget_data= os.path.join("Resources","budget_data.csv")
 
 
@@ -12,11 +14,13 @@ with open(budget_data) as csv_file:
     #pulling the headers
     csv_header = next(csv_file)
 
-    #creating variables
+    #creating variables and lists
     total_months = 0
     total_profits_losses = 0
+    profits_losses = []
+    list_of_changes =[]
+    list_of_months = []
     
-
     for row in csv_reader:
 
         #finding the total months
@@ -25,15 +29,52 @@ with open(budget_data) as csv_file:
         #finding the net total of profits and losses
         total_profits_losses = total_profits_losses + int(row[1])
 
-        #Average change
-       
+        #update list to hold the values in the profit/losses column and list for months
+        profits_losses.append(int(row[1]))
+
+        list_of_months.append(row[0])
+
+    #updating the change list
+    for i in range(1,len(profits_losses)):
+        change = profits_losses[i] - profits_losses[i-1]
+        list_of_changes.append(change)
+    
+    #finding the average change
+    average_change = average(list_of_changes) 
+    round_average_change = round(average_change,2)
+
+    #finding the greatest increase and in what month
+    greatest_increase = max(list_of_changes)
+    
+    increase_index = list_of_changes.index(greatest_increase)
+    greatest_increase_month= list_of_months[increase_index + 1]
+
+    #finding the greatest decrease and in what month
+    greatest_decrease = min(list_of_changes)
+    
+    decrease_index = list_of_changes.index(greatest_decrease)
+    greatest_decrease_month = list_of_months[decrease_index + 1]
+    
+
     #printing the results to the terminal
     print("Financial Analysis")
     print("---------------------------")
     print("Total Months: " + str(total_months))
     print("Total: $" + str(total_profits_losses))
+    print("Average Change: $" + str(round_average_change))
+    print("Greatest Increase in Profits: " + str(greatest_increase_month) + " ($" + str(greatest_increase) + ")")
+    print("Greatest Decrease in Profits: " + str(greatest_decrease_month) + " ($" + str(greatest_decrease) + ")")
+
+
+with open ("analysis/PyBank_analysis.txt", "w") as f:
+    f.write("Financial Analysis" + "\n")
+    f.write("---------------------------" + "\n")
+    f.write("Total Months: " + str(total_months) + "\n")
+    f.write("Total: $" + str(total_profits_losses) + "\n")
+    f.write("Average Change: $" + str(round_average_change) + "\n")
+    f.write("Greatest Increase in Profits: " + str(greatest_increase_month) + " ($" + str(greatest_increase) + ")" + "\n")
+    f.write("Greatest Decrease in Profits: " + str(greatest_decrease_month) + " ($" + str(greatest_decrease) + ")" + "\n")
+    f.close  
+
     
-
-
-        
         
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-challenge
 This is my solution to homework #3 for data boot camp.
+## Credits
+Credit to Sam Espe for the help creating the list to store the changes between profits and losses and Scott Neubauer for creating the list to store the months in a list!
+Thank you to TA Chris for the help creating the index to find the months for the greatest increase and decrease!
